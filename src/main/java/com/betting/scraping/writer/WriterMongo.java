e14eeb0abff2643bfb9efab36226a6e4346b2503
rosariobarresi <r.barresi89@gmail.com>
2017-10-14 17:46:47 +0200

    caricamento partita

--- src/main/java/com/betting/scraping/writer/WriterMongo.java
+++ src/main/java/com/betting/scraping/writer/WriterMongo.java
@@ -2,24 +2,33 @@ package com.betting.scraping.writer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.betting.dao.entity.MatchEntity;
-import com.betting.dao.repository.MatchRepository;
+import com.betting.dao.entity.EurobetMatchEntity;
+import com.betting.dao.repository.EurobetMatchRepository;
 import com.betting.scraping.connection.abstractwriter.WriterAbstract;
 import com.betting.scraping.connection.bean.response.Partita;
 import com.betting.scraping.connection.bean.response.PartiteLega;
 
 public class WriterMongo extends WriterAbstract<PartiteLega> {
 	@Autowired
-	private MatchRepository matchRepository;
+	private EurobetMatchRepository matchRepository;
 
 	@Override
 	public void run() {
 		while (!codaElaborazione.isFinishReader() || !codaElaborazione.isEmpty()) {
 			PartiteLega element = codaElaborazione.dequeue();
 			for (Partita partita : element.getListaPartite()) {
-				MatchEntity matchEntity = new MatchEntity();
-				matchEntity.setMatch(partita.getPartita());
-				matchRepository.insertMatch(matchEntity);
+				EurobetMatchEntity eurobetMatchEntity = new EurobetMatchEntity();
+				eurobetMatchEntity.setMatch(partita.getPartita());
+				eurobetMatchEntity.setAwayWins(partita.getQuotaVittoriaFuoriCasa());
+				eurobetMatchEntity.setDraw(partita.getQuotaPareggio());
+				eurobetMatchEntity.setGoal(partita.getQuotaGoal());
+				eurobetMatchEntity.setHomeWins(partita.getQuotaVittoriaCasa());
+				eurobetMatchEntity.setIdMatch(partita.getPartita().hashCode());
+				eurobetMatchEntity.setNoGoal(partita.getQuotaNoGoal());
+				eurobetMatchEntity.setOver25(partita.getQuoteOver());
+				eurobetMatchEntity.setTime(System.currentTimeMillis());
+				eurobetMatchEntity.setUnder25(partita.getQuotaUnder());
+				matchRepository.insertMatch(eurobetMatchEntity);
 			}
 
 		}
